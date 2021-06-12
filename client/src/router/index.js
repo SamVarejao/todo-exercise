7e09a0e31f5814b9d12ac7993dfd7d20d70bb8ca
SamVarejao <scvarejao@gmail.com>
2021-06-12 11:55:17 +0100

    Secure routing added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,28 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import List from "../views/List.vue";
+import axios from "axios";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+function routeGuardAuth(to, from, next) {
+  // protect routes from logged in users
+
+  axios
+    .get("/api/safeguard") //makes a serverside call to check if the user is logged in
+    .then((response) => {
+      if (response.data.login == true) {
+        next("/list");
+      } else {
+        next();
+      }
+    });
+}
+
+function routeGuard(to, from, next) {
+  // protect routes from non-logged in users
+  function popUp() {
+    alert("Please log in");
+  }
+
+  axios.get("/api/safeguard").then((response) => {
+    if (response.data.login == true) {
+      next();
+    } else {
+      next("/login");
+      popUp();
+    }
+  });
+}
 
 const routes = [
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
+    path: "/",
+    name: "main",
+    beforeEnter: routeGuardAuth,
+    components: {
+      a: Login,
+    },
   },
   {
-    path: '/login',
-    name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Login
-  }
-]
+    path: "/register",
+    name: "Register",
+    beforeEnter: routeGuardAuth,
+    components: {
+      a: Register,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    beforeEnter: routeGuardAuth,
+    components: {
+      a: Login,
+    },
+  },
+  {
+    path: "/list",
+    name: "List",
+    beforeEnter: routeGuard,
+    components: {
+      a: List,
+    },
+  },
+  {
+    path: "*",
+    name: "*",
+    beforeEnter: routeGuardAuth,
+    components: {
+      a: Login,
+    },
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
